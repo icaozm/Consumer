07bdb0c13fea0173653b1911557878dcf6ec5897
czm <1034946681@qq.com>
2023-03-02 23:46:54 +0800

    provider

--- src/main/java/com/czm/consumer/Consumer.java
+++ src/main/java/com/czm/consumer/Consumer.java
@@ -1,9 +1,27 @@
-package com.czm.consumer;/**
-* @author: caozm
-* @create: 2023-03-02 22:24
-* @Version 1.0
-* @description: 
-**/
+package com.czm.consumer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * @author: caozm
+ * @create: 2023-03-02 22:24
+ * @Version 1.0
+ * @description:
+ **/
+@RestController
 public class Consumer {
+
+    @Autowired
+    private RestTemplate restTemplate;
+
+
+    @GetMapping("/echo")
+    public String echo() {
+        return restTemplate.getForObject("http://provider/hello/t",String.class);
+    }
+
 
 }
--- src/main/java/com/czm/consumer/ConsumerApplication.java
+++ src/main/java/com/czm/consumer/ConsumerApplication.java
@@ -1,13 +1,30 @@
 package com.czm.consumer;
 
+import com.alibaba.cloud.nacos.ribbon.NacosRule;
+import com.netflix.loadbalancer.IRule;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ConsumerApplication {
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConsumerApplication.class, args);
-	}
+    @Bean
+    public IRule ribbonRule() {
+        return new NacosRule();
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConsumerApplication.class, args);
+    }
 
 }
