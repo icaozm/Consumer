8e3045c5e95c97248cda0b085974e2f48e855bd4
czm <1034946681@qq.com>
2023-03-02 23:46:10 +0800

    Initial commit

---
+++ src/main/java/com/czm/consumer/Consumer.java
@@ -0,0 +1,9 @@
+package com.czm.consumer;/**
+* @author: caozm
+* @create: 2023-03-02 22:24
+* @Version 1.0
+* @description: 
+**/
+public class Consumer {
+
+}
---
+++ src/test/java/com/czm/consumer/ConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.czm.consumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
